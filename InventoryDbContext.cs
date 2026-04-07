218e8b04f93be79b24b0b76b80a7876936afa2c9
Muhammad Sarosh <muhammadsarosh.dev@gmail.com>
2026-04-07 08:08:21 +0500

    feat: add initial EF Core migration and create InventoryApiDb schema

--- InventoryDbContext.cs
+++ InventoryDbContext.cs
@@ -4,10 +4,15 @@ namespace InventoryApi
 {
     public class InventoryDbContext : DbContext
     {
-        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
-        {
-        }
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
     }
 }
